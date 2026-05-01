bbecaaaa7ae51b1a190d40386ebe77a35c0bc72b
StoicChimera <asuan@praetorsol.com>
2026-05-01 08:00:57 -0600

    fix: add src to python path for streamlit

--- src/nautical_dashboard/main.py
+++ src/nautical_dashboard/main.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
 import streamlit as st
+
 from nautical_dashboard.app.modules import (
     revenue,
     allocations,
